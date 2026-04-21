d2e18a2e099016029d87f0d772e1fe0d443af89e
Арина Цветнова <cvetnovaarina@gmail.com>
2026-04-21 14:34:13 +0300

    Созданны XML-комментарии ветки "Буквы"

--- pr7/Program.cs
+++ pr7/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
-
+/// <summary>
+/// Класс, демонстрирующий работу с массивами символов и целых чисел, а также передачу данных в метод.
+/// </summary>
 class ArrayExample
 {
+    /// <summary>
+    /// Точка входа в приложение: формирует строку из символов массива, заполняет числовой массив и выводит приветствия.
+    /// </summary>
     static void Main()
     {
         char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
@@ -15,7 +20,11 @@ class ArrayExample
         }
         Console.ReadKey();
     }
-
+    /// <summary>
+    /// Выводит в консоль персонализированное приветствие с указанием имени и числа для счёта.
+    /// </summary>
+    /// <param name="name">Строка с именем, которое подставляется в приветствие.</param>
+    /// <param name="msg">Целое число, указывающее верхнюю границу счёта в сообщении.</param>
     static void SendMessage(string name, int msg)
     {
         Console.WriteLine("Hello, " + name + "! Count to " + msg);

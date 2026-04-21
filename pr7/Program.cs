df7e5bf159f5ccd2b958a5dbf11caf8987d225ba
Арина Цветнова <cvetnovaarina@gmail.com>
2026-04-21 14:18:21 +0300

    Созданны XML-комментарии ветви "Фибоначчи"

--- pr7/Program.cs
+++ pr7/Program.cs
@@ -2,14 +2,25 @@
 
 namespace pr7
 {
+    /// <summary>
+    /// Главный класс приложения.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Точка входа в приложение: вычисляет 5-е число Фибоначчи и выводит результат в консоль.
+        /// </summary>
+        /// <param name="args">Аргументы командной строки, переданные при запуске.</param>
         static void Main(string[] args)
         {
             int result = Fibonacci(5);
             Console.WriteLine(result);
         }
-
+        /// <summary>
+        /// Вычисляет n-е число последовательности Фибоначчи итеративным методом.
+        /// </summary>
+        /// <param name="n">Порядковый номер числа в последовательности Фибоначчи начиная с 0.</param>
+        /// <returns>Целочисленное значение n-го числа Фибоначчи.</returns>
         static int Fibonacci(int n)
         {
             Console.WriteLine("The output is: ");
